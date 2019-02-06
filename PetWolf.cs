7dd2f74f1a2559117f350b06b254737403ad2056
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-02-06 13:46:18 +0200

    Add files via upload

---
+++ PetWolf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+     class PetWolf : Wolf
+    {
+        public PetWolf(string name, string FavoritFood, string nameOfPack) : base(name, FavoritFood, nameOfPack)
+        {
+        }
+
+        public override void Bark()
+        {
+            Console.WriteLine(".......");
+        }
+    }
+}
